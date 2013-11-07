cf8651e54ca0d3a78c7cba007c2797dcc5c6bf52
bjh <brodie.hanbuch@gmail.com>
2013-11-07 10:45:17 -0500

    prototyping the wizard view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magic::Application.config.secret_key_base = '70a74a894ea5a18c35f5a2179d9cf363ce19fccf625743b63becab9b37db00edf75d55fe863c0ed86e9fb502185ca5704822fc3cefec4e9e4ff0b4d0b091487e'
